bc096e4e1f5df14d84e5009b6d5f9237945299f1
Ahkhan1815 <ahkhan1815@gmail.com>
2024-12-25 18:58:47 -0600

    more backend fix

--- routes/Comments.js
+++ routes/Comments.js
@@ -15,8 +15,8 @@ router.post("/", validateToken, checkVerification, async (req, res) => {
     const comment = req.body;
     const username = req.user.username;
     comment.username = username;
-    await Comments.create(comment);
-    res.json(comment);
+    const newComment = await Comments.create(comment);
+    res.json(newComment);
 });
 
 router.delete("/:commentId", async (req, res) => {
