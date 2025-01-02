2d5c98010062a0ca2e1cf7bdf3a06708e26260a7
Ahkhan1815 <ahkhan1815@gmail.com>
2025-01-01 21:33:03 -0600

    fetch feature

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,7 +1,26 @@
 const {verify} = require("jsonwebtoken");
 const {jwtDecode} =  require("jwt-decode");
+const { Users } = require("../models");
+require('dotenv').config();
 
-const checkVerification = (req,res,next) => {
+const checkVerification = async (req,res,next) => {
+    const accessToken = req.header("accessToken");
+    try{
+        const validToken = jwtDecode(accessToken);
+        const user = await Users.findbyPk(validToken.id);
+        if (user.verification){
+            return next();
+        }
+        else{
+        res.json({error: "Account not verified. Please click the link sent to your email to verify this account."});
+        }
+    }
+    catch (err) {
+    return res.json({error: err});
+    }
+}
+
+    /*
     const accessToken = req.header("accessToken");
     try{
         const validToken = jwtDecode(accessToken);
@@ -15,7 +34,8 @@ const checkVerification = (req,res,next) => {
     } catch (err) {
         return res.json({error: err});
     }
-}
+        */
+
 
 const validateToken = (req,res,next) => {
     const accessToken = req.header("accessToken");
@@ -23,7 +43,7 @@ const validateToken = (req,res,next) => {
     if (!accessToken) return res.json({error: "User not logged in"});
 
     try {
-        const validToken = verify(accessToken, "xvVOShLivM");
+        const validToken = verify(accessToken, process.env.JWT_CODE);
         const username = validToken.username;
         req.user = validToken;
 
